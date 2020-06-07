d32211c9d5ec1ba36ab78c96810188b58310c621
Irwanto <irwantojo@gmail.com>
2020-06-07 13:09:42 +0700

    change api url for production

--- utils/axiosApi.js
+++ utils/axiosApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://personal-project-management.now.sh/'
+    ? 'https://project-management-6slcxy5zp.now.sh/'
     : 'http://localhost:3000/';
 
 export default axios.create({
