0a6e1a08aaf3252fea039a5d0f350e4c09bc8e5a
Irwanto <irwantojo@gmail.com>
2020-06-07 13:39:06 +0700

    change connection string to hard coded constant

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 const connection = {};
 
+const connection =
+  'mongodb://ajak:ajak123@cluster0-shard-00-00-xpsoz.mongodb.net:27017,cluster0-shard-00-01-xpsoz.mongodb.net:27017,cluster0-shard-00-02-xpsoz.mongodb.net:27017/ppm?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
+
 async function db() {
   if (connection.isConnected) {
     // Use existing database connection
@@ -8,7 +11,7 @@ async function db() {
     return;
   }
   // Use new database connection
-  const db = await mongoose.connect(process.env.MONGO_SRV, {
+  const db = await mongoose.connect(connection, {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
